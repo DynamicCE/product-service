31d9fe15b84c3d568bb0a9dc7dc32926c969e5ff
Adınız <erkanmurat0632@gmail.com>
2024-11-08 14:56:59 +0300

    test class created

--- src/test/java/com/erkan/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/erkan/product_service/ProductServiceApplicationTests.java
@@ -3,12 +3,26 @@ package com.erkan.product_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
 		"spring.data.mongodb.uri=mongodb://localhost:27017/test-db"
 })
+@Testcontainers
 class ProductServiceApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
 	void contextLoads() {
